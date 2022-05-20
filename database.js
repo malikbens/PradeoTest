0169fb42de95e61cacb02013ca31e0a3b0a5ede9
Malik <malikbens@live.fr>
2022-05-20 20:07:54 +0200

    adding apk app to mysql

--- database.js
+++ database.js
@@ -1,20 +1,29 @@
-db.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
-  const sql = ("CREATE TABLE IF NOT EXISTS files (id INT(10) NOT NULL AUTO_INCREMENT,name TEXT,);");
-  db.query("CREATE DATABASE IF NOT EXISTS pradeo;", function (err, result) {
-      if (err) throw err;
-      console.log("Database created");
-      db.query("USE pradeo;", function (err, results) {
-          if (err) throw err;
-          console.log("Database connected");
-      });
-      db.query(sql, function (err, result) {
-          if (err) throw err;
-          console.log("Table created");
-      })
-  });
-});
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host:"localhost",
+  user:"root",
+  password:"",
+  database:"pradeo"
+})
+
+// db.connect(function (err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   const sql = ("CREATE TABLE IF NOT EXISTS files (id INT(10) NOT NULL AUTO_INCREMENT,name TEXT,);");
+//   db.query("CREATE DATABASE IF NOT EXISTS pradeo;", function (err, result) {
+//       if (err) throw err;
+//       console.log("Database created");
+//       db.query("USE pradeo;", function (err, results) {
+//           if (err) throw err;
+//           console.log("Database connected");
+//       });
+//       db.query(sql, function (err, result) {
+//           if (err) throw err;
+//           console.log("Table created");
+//       })
+//   });
+// });
 
 db.connect(function(err){
   if (err) throw err;
--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser')
+const cors = require('cors');
+const db = require('./database')
 const path = require('path');
 
 const app = express();
 const port = 5000
+
+// enable CORS
+app.use(cors());
+// parse application/json
+app.use(bodyParser.json());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/uploads', express.static('uploads'));
 
 app.set('view engine', 'ejs')
 
@@ -17,18 +28,24 @@ app.get('/', async (req, res, next) => {
     res.render("index")
 })
 
-app.post('/single', async(req, res, next) => {
+app.post('/single', async (req, res, next) => {
     try {
         const file = req.files.mFile;
         console.log(file);
-        const fileName = new Date().getTime().toString()+path.extname(file.name);
-        const savePath = path.join(__dirname, 'public','uploads', fileName);
-        if(file.mimetype !== 'application/vnd.android.package-archive'){
+        const fileName = new Date().getTime().toString() + path.extname(file.name);
+        const savePath = path.join(__dirname, 'public', 'uploads', fileName);
+        if (file.mimetype !== 'application/vnd.android.package-archive') {
             throw new Error('Only adroid applications are supported')
         }
         await file.mv(savePath);
         res.redirect('/')
-        
+
+        var sql = "INSERT INTO `files`(`name`) VALUES ('" + fileName + "')";
+        db.query(sql, function (err, res) {
+            if (err) throw err
+            console.log('success')
+        });
+
     } catch (error) {
         console.log(error)
         res.send("Error uploading file")
@@ -39,18 +56,19 @@ app.post('/multiple', async (req, res, next) => {
     try {
         const files = req.files.mFiles;
         const promises = files.map(file => {
-            const savePath = path.join(__dirname,'public','uploads',file.name)
+            const savePath = path.join(__dirname, 'public', 'uploads', file.name)
             return file.mv(savePath)
         })
 
         await Promise.all(promises);
         res.redirect('/')
 
-    } catch (error){
-
+    } catch (error) {
+        console.log(error)
+        res.send("Error uploading file")
     }
 })
 
 app.listen(port, () => {
     console.log(`running on port ${port}`)
-  })
+})
