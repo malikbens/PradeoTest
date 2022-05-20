18f14721f8fa1ce6a8c5df1584a16e40a6ce4305
Malik <malikbens@live.fr>
2022-05-20 13:43:33 +0200

    express server good to go

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const package = require('./package.json');
+
 
 const port = process.env.PORT || 5000;
+const apiRoot = '/api';
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -10,6 +15,15 @@ app.use(cors({
 }));
 app.options('*', cors());
 
+// configure routes
+const router = express.Router();
+router.get('/', (req, res) => {
+    res.send(`${package.name} -v${package.version}`);
+});
+
+// register routes
+app.use(apiRoot, router);
+
 app.listen(port, () => {
     console.log('Server is up !')
-})
+});
